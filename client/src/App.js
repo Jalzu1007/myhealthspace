39fddddcefadc7620af3f144228e8bf9f9a111b3
Joshua <23joshtc@gmail.com>
2023-09-07 17:48:05 -0400

    Add compenents to App.js and imported them as well

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import AppNavbar from './components/Navbar';
 import LoginForm from './components/LoginForm';
-import Navbar from './components/Navbar';
 import SignupForm from './components/SignupForm';
 
 
@@ -10,20 +10,9 @@ import SignupForm from './components/SignupForm';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {<AppNavbar/>}
+      {<LoginForm/>}
+      {<SignupForm/>}
     </div>
   );
 }
