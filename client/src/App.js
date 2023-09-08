be3a58c4eea5ed3a8df8a11dc7c3331c1ce5e73b
jimen8930 <jimen8930@aol.com>
2023-09-07 21:19:29 -0400

    Updated to the app.js file to set up the Apollo Client to handle GraphQL requests and state management.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,38 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import AppNavbar from './components/Navbar';
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
 import Homepage from './components/Homepage';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 
 
@@ -16,12 +45,16 @@ function App() {
   };
 
   return (
-    <div className="App">
-  // <AppNavbar onNavClick={handleNavClick} /> 
-      {<Homepage/>}
-      {<LoginForm/>}
-      {<SignupForm/>}
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <AppNavbar onNavClick={handleNavClick} />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/signup" component={SignupForm} />
+        </Switch>
+      </Router>
+    </ApolloProvider>
   );
 }
 
