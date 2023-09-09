40a1c3c3576bb9020023125c2cfa55eb41e2ec32
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-09-08 20:23:34 -0400

    added typeDefs code with mutation and query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,28 +1,57 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Tech {
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    cardio: [Cardio]
+    resistance: [Resistance]
+  }
+
+  type Resistance {
     _id: ID!
     name: String!
+    type: String!
+    weight: Float!
+    sets: Int!
+    reps: Int!
+    date: Date!
+    userId: ID!
   }
 
-  type Matchup {
+  type Cardio {
     _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
+    type: String!
+    name: String!
+    distance: Float!
+    duration: Float!
+    date: Date!
+    userId: ID!
   }
 
-  type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
+  type Auth {
+    token: String!
+    user: User!
   }
+  
+  type Query {
+  getCardioById(cardioId: ID!): Cardio
+  getResistanceById(resistanceId: ID!): Resistance
+  getUser: User
+}
 
   type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
-    createVote(_id: String!, techNum: Int!): Matchup
-  }
+  createCardio(cardioInput: CardioInput!): Cardio
+  deleteCardio(cardioId: ID!): Cardio
+  updateCardio(cardioId: ID!, cardioInput: CardioInput!): Cardio
+  createResistance(resistanceInput: ResistanceInput!): Resistance
+  deleteResistance(resistanceId: ID!): Resistance
+  updateResistance(resistanceId: ID!, resistanceInput: ResistanceInput!): Resistance
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+}
 `;
 
 module.exports = typeDefs;
