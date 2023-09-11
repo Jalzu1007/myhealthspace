ba84240e196c71048392f076fe95c734c2b46c57
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-09-11 15:55:46 -0400

    added mutation file to src

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,23 +1,111 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_MATCHUP = gql`
-  mutation createMatchup($tech1: String!, $tech2: String!) {
-    createMatchup(tech1: $tech1, tech2: $tech2) {
-      _id
-      tech1
-      tech2
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        savedWorkouts{
+          ... on Cardio {
+            _id
+            type
+            name
+            distance
+            duration
+            date
+          }
+          ... on Resistance {
+            _id
+            type
+            name
+            weight
+            sets
+            reps
+            date
+          }
+        }
+      }
     }
   }
 `;
 
-export const CREATE_VOTE = gql`
-  mutation createVote($_id: String!, $techNum: Int!) {
-    createVote(_id: $_id, techNum: $techNum) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+export const CREATE_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
+  }
+`;
+
+export const CREATE_CARDIO = gql`
+  mutation createCardio($cardioInput: CardioInput!) {
+    createCardio(cardioInput: $cardioInput) {
+      _id
+      type
+      name
+      distance
+      duration
+      date
+    }
+  }
+`;
+
+export const UPDATE_CARDIO = gql`
+  mutation updateCardio($_id: ID!, $cardioInput: CardioInput!) {
+    updateCardio(_id: $_id, cardioInput: $cardioInput) {
+      _id
+      type
+      name
+      distance
+      duration
+      date
+    }
+  }
+`;
+
+export const DELETE_CARDIO = gql`
+  mutation deleteCardio($_id: ID!) {
+    deleteCardio(_id: $_id)
+  }
+`;
+
+export const CREATE_RESISTANCE = gql`
+  mutation createResistance($resistanceInput: ResistanceInput!) {
+    createResistance(resistanceInput: $resistanceInput) {
+      _id
+      name
+      type
+      weight
+      sets
+      reps
+      date
+    }
+  }
+`;
+
+export const UPDATE_RESISTANCE = gql`
+  mutation updateResistance($_id: ID!, $resistanceInput: ResistanceInput!) {
+    updateResistance(_id: $_id, resistanceInput: $resistanceInput) {
+      _id
+      name
+      type
+      weight
+      sets
+      reps
+      date
+    }
+  }
+`;
+
+export const DELETE_RESISTANCE = gql`
+  mutation deleteResistance($_id: ID!) {
+    deleteResistance(_id: $_id)
   }
 `;
