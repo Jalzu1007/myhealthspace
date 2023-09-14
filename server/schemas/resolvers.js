4f7437beb27c9ced0780915e4dc2db4cacf08958
Jose Alzuri <jalzu1007@gmail.com>
2023-09-14 18:33:01 -0400

    Andrea refactored typedefs and resolvers adding mutation and queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,23 +1,17 @@
-const { User, Cardio, Resistance } = require('../models');
+const { User, Workouts } = require('../models');
 const { signToken } = require("../utils/auth");
 const { AuthenticationError } = require("apollo-server-express");
-
+​
 const resolvers = {
   Query: {
-   user: async (parent, args, context) => {
+   getUser: async (parent, args, context) => {
       if (!context.user) {
         const user = await User.findById(context.user._id);
         return user;
       }
       throw new AuthenticationError('User not authenticated');
     },
-    cardio: async (parent, { _id }) => {
-      return await Cardio.findById(_id);
-    },
-    resistance: async (parent, { _id }) => {
-      return await Resistance.findById(_id);
-    },
-    userWorkouts: async (_, { userId }) => {
+    listWorkouts: async (_, { userId }) => {
       const user = await User.findById(userId).populate('savedWorkouts');
       return user.savedWorkouts;
     },
@@ -28,148 +22,111 @@ const resolvers = {
       if (!user) {
         throw new AuthenticationError('Incorrect credentials');
       }
-
+​
       const correctPw = await user.isCorrectPassword(password);
       if (!correctPw) {
         throw new AuthenticationError('Incorrect credentials');
       }
       const token = signToken(user);
-
+​
       return { token, user };
   },
     createUser: async (parent, args) => {
       const user = await User.create(args);
       const token = signToken(user);
-
+​
       return { token, user };
   },
-    createCardio: async (parent, { cardioInput }, context) => {
-      if (!context.user) {
-        throw new AuthenticationError('User not authenticated');
-      }
+    createWorkout: async (parent, { input }, context) => {
     
-      cardioInput.userId = context.user._id;
-      const cardio = await Cardio.create(cardioInput);
-      const user = await User.findByIdAndUpdate(
-        context.user._id,
-        { $push: { savedWorkouts: cardio._id } },
-        { new: true }
-      );
-
-      return cardio;
-  },
-    updateCardio: async (parent, { _id, cardioInput }, context) => {
-      if (!context.user) {
-        throw new AuthenticationError('User not authenticated');
-      }
-    
-      const existingCardio = await Cardio.findById(_id);
-      if (existingCardio.userId.toString() !== context.user._id.toString()) {
-        throw new AuthenticationError('Unauthorized');
-      }  
-
-      const updatedCardio = await Cardio.findByIdAndUpdate(_id, cardioInput,
-         { new: true });
-    //update in user's profile as well
-      await User.findByIdAndUpdate(
-        context.user._id,
-        { $set: { 'savedWorkouts.$[elem]': updatedCardio } },
-        { arrayFilters: [{ 'elem._id': _id }], new: true }
-      );
+    if (!context.user) {
+      throw new AuthenticationError('User not authenticated');
+    }
+​
+    const workout = new Workouts({
+      ...input,
+      userId: context.user._id,
+    });
+​
   
-      return updatedCardio;
+    await workout.save();
+​
+    await User.findByIdAndUpdate(
+      context.user._id,
+      { $push: { savedWorkouts: workout._id } },
+      { new: true }
+    );
+​
+    return workout;
   },
-    deleteCardio: async (parent, { _id }, context) => {
+​
+    updateWorkout: async (parent, { id, input }, context) => {
       if (!context.user) {
         throw new AuthenticationError('User not authenticated');
       }
     
-      const existingCardio = await Cardio.findById(_id);
-      if (existingCardio.userId.toString() !== context.user._id.toString()) {
+      const existingWorkout = await Workouts.findById(id);
+    
+      if (!existingWorkout || existingWorkout.userId.toString() !== context.user._id.toString()) {
         throw new AuthenticationError('Unauthorized');
       }
     
-      await Cardio.findByIdAndRemove(_id);
+      const updatedWorkout = await Workouts.findByIdAndUpdate(id, input, { new: true });
+    
+      const user = await User.findById(context.user._id);
+    
+      const savedWorkouts = user.savedWorkouts.map((savedWorkout) => {
+        if (savedWorkout._id.toString() === id) {
+          if (input.name) savedWorkout.name = input.name;
+          if (input.weight) savedWorkout.weight = input.weight;
+          if (input.sets) savedWorkout.sets = input.sets;
+          if (input.reps) savedWorkout.reps = input.reps;
+          if (input.date) savedWorkout.date = input.date;
+        }
+        return savedWorkout;
+      });
+    
+      user.savedWorkouts = savedWorkouts;
+    
+      // Save the updated user document
+      await user.save();
+    
+      return updatedWorkout;
+    },
+    
+    deleteWorkout: async (parent, { id }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError('User not authenticated');
+      }
+​
+      const existingWorkout = await Workouts.findById(id);
+​
+      if (!existingWorkout || existingWorkout.userId.toString() !== context.user._id.toString()) {
+        throw new AuthenticationError('Unauthorized');
+      }
+​
+      await Workouts.findByIdAndRemove(id);
+​
       await User.findByIdAndUpdate(
         context.user._id,
-        { $pull: { savedWorkouts: _id } },
+        { $pull: { savedWorkouts: id } },
         { new: true }
       );
-    
-      return 'Cardio workout deleted successfully';
-  },
-    createResistance: async (parent, { resistanceInput }, context) => {
-      if (!context.user) {
-        throw new AuthenticationError('User not authenticated');
-      }
-    
-      resistanceInput.userId = context.user._id;
-      const resistance = await Resistance.create(resistanceInput);
-    //adding to user's profile
-      const user = await User.findByIdAndUpdate(
-        context.user._id,
-        { $push: { savedWorkouts: resistance._id } },
-        { new: true }
-      );
-    
-      return resistance;
-  },
-    updateResistance: async (parent, { _id, resistanceInput }, context) => {
-      if (!context.user) {
-        throw new AuthenticationError('User not authenticated');
-      }
-    
-      const existingResistance = await Resistance.findById(_id);
-      if (existingResistance.userId.toString() !== context.user._id.toString()) {
-        throw new AuthenticationError('Unauthorized');
-      }
-    
-      const updatedResistance = await Resistance.findByIdAndUpdate(
-        _id,
-        resistanceInput,
-        { new: true }
-      );
-      //updates user's profile with updatedresistance
-      const updatedUser = await User.findOneAndUpdate(
-        {
-          _id: context.user._id,
-          'savedWorkouts._id': _id,
-        },
-        {
-          $set: {
-            'savedWorkouts.$.name': updatedResistance.name,
-            'savedWorkouts.$.weight': updatedResistance.weight,
-            'savedWorkouts.$.sets': updatedResistance.sets,
-            'savedWorkouts.$.reps': updatedResistance.reps,
-            'savedWorkouts.$.date': updatedResistance.date,
-          },
-        },
-        { new: true }
-      );
-    
-      return updatedResistance;
-  },
-    deleteResistance: async (_, { _id }, context) => {
-      if (!context.user) {
-        throw new AuthenticationError('User not authenticated');
-      }
-    
-      const existingResistance = await Resistance.findById(_id);
-      if (existingResistance.userId.toString() !== context.user._id.toString()) {
-        throw new AuthenticationError('Unauthorized');
-      }
-    
-      const deletedResistance = await Resistance.findByIdAndRemove(_id);
-      //remove from user's savedWorkouts
-      await User.findByIdAndUpdate(
-        context.user._id,
-        { $pull: { savedWorkouts: _id } },
-        { new: true }
-      );
-    
-      return deletedResistance;
-  },
-  },
+​
+      return savedWorkouts; 
+    },
+},
 };
-
+​
 module.exports = resolvers;
+Collapse
+
+
+
+
+
+
+
+
+
+
